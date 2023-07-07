e8f82b66c922e401eaad6a2cc12943006e3cd695
yourarj <11359226+yourarj@users.noreply.github.com>
2023-07-07 22:14:50 +0530

    first simple macro use in demo app
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Fri Jul 7 22:14:50 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   macro-demo-app/Cargo.toml
    	modified:   macro-demo-app/src/main.rs

--- macro-demo-app/src/main.rs
+++ macro-demo-app/src/main.rs
@@ -1,3 +1,13 @@
+use am::simple_func_like_macro;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Macro-World!");
+
+    // try to invoke the function which simple_func_like_macro declared
+    let lucky_num = give_me_the_lucky_number();
+
+    println!("The number from func like macro: {}", lucky_num);
 }
+
+// let the macro create a function definition
+simple_func_like_macro!(everything, in, here, will, be, ignored);
